d0c7c77e0f38cb8c2056ac63ca67a56131db24cb
sochi419 <equipment2241@gmail.com>
2023-08-03 13:13:39 +0900

    conflict対応

--- app/models/user.rb
+++ app/models/user.rb
@@ -742,6 +742,26 @@ class User < ApplicationRecord
     )
   end
 
+  def automatic_retire_datetime
+    return unless hibernated_at
+    hibernated_at.advance(months: 3)
+  end
+
+  def remaining_days_until_automatic_retire
+    return unless hibernated_at
+    ((hibernated_at.advance(months: 3) - Time.zone.now) / 86_400).floor
+  end
+
+  def remaining_hours_until_automatic_retire
+    return unless hibernated_at
+    ((hibernated_at.advance(months: 3) - Time.zone.now) / 3600).floor
+  end
+
+  def remaining_minutes_until_automatic_retire
+    return unless hibernated_at
+    ((hibernated_at.advance(months: 3) - Time.zone.now) / 60).floor
+  end
+
   private
 
   def password_required?
